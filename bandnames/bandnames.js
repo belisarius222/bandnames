80195a6319a658a9731753b9452235e69ba11d59
Ted Blackman <ted@zigfu.com>
2012-07-15 05:15:39 -0400

    removed another vestigial comment to test git

--- bandnames/bandnames.js
+++ bandnames/bandnames.js
@@ -12,7 +12,6 @@ if (Meteor.is_client) {
 
   Template.submitname.events = {
     'click input' : function () {
-      // template data, if any, is available in 'this'
       if (typeof console !== 'undefined'){
         console.log("You pressed the button");
       }
