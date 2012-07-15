c7d392bc3478eb0c8387be56c27125c413dce656
Ted Blackman <ted.blackman@gmail.com>
2012-07-14 21:37:14 -0700

    First try at displaying band name list

--- bandnames/bandnames.js
+++ bandnames/bandnames.js
@@ -1,19 +1,35 @@
+BandNames = new Meteor.Collection("bandnames");
+
 if (Meteor.is_client) {
-  Template.hello.greeting = function () {
-    return "Welcome to bandnames.";
+  Template.names.greeting = function () {
+    return "Yet another silly thing on the internet.";
   };
 
-  Template.hello.events = {
+  Template.submitname.events = {
     'click input' : function () {
       // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
+      if (typeof console !== 'undefined'){
         console.log("You pressed the button");
+        BandNames.insert({'text': new_bandname, 'author': new_author });
+      }
     }
   };
 }
 
 if (Meteor.is_server) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+  Meteor.startup(function(){
+    if (BandNames.find().count() === 0) {
+      var names = [
+        {text: 'DangleSkank',   author: 'Joel'},
+        {text: 'WrathPony',     author: 'Joel'},
+        {text: 'HMS Groovytron',author: 'Cody'},
+        {text: 'wubwub',        author: 'Sol', times_suggested: 2}
+      ];
+    }
+    for (var i = 0; i < names.length; i++){
+      BandNames.insert(names[i],function(){
+        console.log('added initial names array');
+      });
+    }
   });
 }
