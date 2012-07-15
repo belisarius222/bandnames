5d7432336e20bff52a0dfecd376c9d2db19e34b3
Ted Blackman <ted@zigfu.com>
2012-07-15 04:36:39 -0400

    Moved code out of silly conditional

--- bandnames/bandnames.js
+++ bandnames/bandnames.js
@@ -18,10 +18,10 @@ if (Meteor.is_client) {
       // template data, if any, is available in 'this'
       if (typeof console !== 'undefined'){
         console.log("You pressed the button");
-        var text = $('[name="new_bandname"]').val();
-        var author = $('[name="new_bandname"]').val();
-        BandNames.insert({'text': text, 'author': author });
       }
+      var text = $('[name="new_bandname"]').val();
+      var author = $('[name="new_bandname"]').val();
+      BandNames.insert({'text': text, 'author': author });
     }
   };
 }
